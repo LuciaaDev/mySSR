afd9f34905c388c8d33e3b2afb2b444918f27039
Lucía Ruiz <luciaruiz9722@gmail.com>
2024-10-25 14:37:44 +0200

    feat: add logic of afterNextRender and afterRender (DAY-7)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { afterNextRender, afterRender, Component, inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataService } from './data.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +11,32 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private readonly platformId = inject(PLATFORM_ID);
   private readonly dataSvc = inject(DataService);
   product$ = this.dataSvc.getProduct();
+
+  constructor() {
+    console.log('This platformId is:', this.platformId);
+    // Estos dos ifs es una forma casera de ejecutar afterNextRender y afterRender.
+    // afterNextRender
+    // if(isPlatformBrowser(this.platformId)) {
+    //   localStorage.setItem('cart', 'dominicode');
+    //   console.log("isPlatformBrowser:", this.platformId);
+    // }
+
+    // afterRender
+    // if(isPlatformServer(this.platformId)) {
+    //   console.log("isPlatformServer:", this.platformId);
+    // }
+
+    afterNextRender(() => {
+      localStorage.setItem('cart', 'dominicode');
+      console.log("isPlatformBrowser:", this.platformId);
+    });
+
+    afterRender(() => {
+      // Con este método estamos 100% seguros de que nuestra aplicación/nuestros componentes se han renderizado.
+      console.log("isPlatformServer:", this.platformId);
+    });
+  }
 }
